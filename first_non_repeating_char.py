91f6ff7f66e2e24c5dd3ee567536e15e69c67e8c
Charles <charlespatel07@gmail.com>
2019-06-03 23:39:05 -0700

    First non repeat Constant time

--- first_non_repeating_char.py
+++ first_non_repeating_char.py
@@ -1,9 +1,22 @@
 #Given a string, find its first non-repeating character
-#Time Complexity: O(n)
 
-def non_repeat_char(S):
+#Time Complexity: O(1)
+def non_repeat_char_linear_time(stream):
+    repeated = [False] * 256
+    DLL = []
+
+    for char in stream:
+        if not repeated[ord(char)]:
+            if char not in DLL:
+                DLL.append(char)
+            else:
+                DLL.remove(char)
+    return DLL[0]
+
+#Time Complexity: O(n)
+def non_repeat_char(stream):
     counter = {}
-    for char in S:
+    for char in stream:
         if char not in counter:
             counter[char] = 1
         else:
@@ -12,5 +25,6 @@ def non_repeat_char(S):
         if(v == 1):
             return k
 
-S = 'happybirthday'
-print(non_repeat_char(S))
+stream = 'happybirthday'
+print(non_repeat_char(stream))
+print(non_repeat_char_linear_time(stream))
