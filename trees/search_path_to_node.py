0f860377059ff465e3c1f359c33282b80921ff64
Charles <charlespatel07@gmail.com>
2019-06-06 17:24:21 -0700

    trees algorithm

---
+++ trees/search_path_to_node.py
@@ -0,0 +1,16 @@
+#Print path from root to all nodes in a Complete Binary Tree
+
+def find_path_to_node(paths, root, N):
+    if(root > N):
+        return
+    paths.append(root)
+
+    for i in range(0,len(paths)):
+        print(paths[i], end='\n')
+    print()
+    find_path_to_node(paths[:], root * 2, N)
+    find_path_to_node(paths[:], root * 2 + 1, N)
+
+N = 7
+paths = []
+print(find_path_to_node(paths, 1, N))
