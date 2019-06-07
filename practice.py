879cb93c116c023eabe9d1650a49f20b7b9fac9a
Charles <charlespatel07@gmail.com>
2019-06-06 17:28:09 -0700

    deleted:    ABSorted.py 	deleted:    first_non_repeating_char.py 	deleted:    heap.py 	deleted:    k_closest_number.py 	deleted:    max_sliding_window.py 	deleted:    merge.py 	deleted:    merged_three_sorted_array.py 	deleted:    merged_two_sorted_array.py 	deleted:    monk_multiplication.py 	modified:   practice.py 	deleted:    sort_rotated_array.py 	deleted:    thrice_occuring.py 	deleted:    thrice_occuring_logn.py

--- practice.py
+++ practice.py
@@ -5,3 +5,12 @@ A = [3,8,4,6,2,1,9]
 
 print(list(itertools.accumulate(A, lambda r,y: r+y)))
 print(functools.reduce(lambda x,y: x+y, A))
+
+def yieldfunc():
+    yield 1
+    yield 2
+    yield 32
+
+
+for i in yieldfunc():
+    print(i)
