48a843fa49c90b1fa40a073e8d47b6e06cec53ac
Charles <charlespatel07@gmail.com>
2019-06-09 00:32:30 -0700

    two string in n

--- String/two_string_substring.py
+++ String/two_string_substring.py
@@ -1,5 +1,6 @@
 #Check if two strings have a common substring
 
+#Time Complexity: O(nˆ2)
 def two_string(a, b):
     flag = False
     for i in range(len(a)):
@@ -9,6 +10,19 @@ def two_string(a, b):
                 return flag
     return flag
 
+#Time Complexity: O(n)
+def two_string_n(a, b):
+    v = [0] * 26
+    flag = False
+    for i in a:
+        v[ord(i) - ord('a')] += 1
+    for j in b:
+        if(v[ord(j) - ord('a')] >= 1):
+            flag = True
+            return flag
+    return flag
+
 a = 'chicken'
-b = 'chilli'
+b = 'jkl'
 print(two_string(a, b))
+print(two_string_n(a, b))
