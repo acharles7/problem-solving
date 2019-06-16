be0728e08ae4c188905feb08237f32e5674cdafe
Charles <charlespatel07@gmail.com>
2019-06-16 00:30:20 -0700

    median space complexity

--- Sorting/median_two_sorted_array.py
+++ Sorting/median_two_sorted_array.py
@@ -28,7 +28,25 @@ def median(M):
     else:
         return (M[mid] + M[mid-1]) / 2
 
+def median(A, B):
+    flag = False
+    n, m = len(A), len(B)
+    mid = (n + m)//2
+    print(mid)
+    count = 0
+    i,j = 0,0
+    while(i < n and j < m and count <= mid):
+        if(A[i] <= B[j]):
+            i += 1
+            count += 1
+        else:
+            j += 1
+            count += 1
+    print("Count:",count)
+
+
 A = [-4, -1, 0, 4, 7, 11, 30]
 B = [-8, 0, 1, 30, 32, 40]
 C = merge_two_array(A, B)
-print("Median: ",median(C))
+print(median(A, B))
+#print("Median: ",median(C))
