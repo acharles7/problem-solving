20669f0c6b68fd38f3e18c8b8bbd3741124c00f0
Charles <charlespatel07@gmail.com>
2019-06-18 19:48:20 -0700

    two sum 2

--- Sorting/two_sum.py
+++ Sorting/two_sum.py
@@ -1,3 +1,5 @@
+#two sum
+
 def two_sum(A, target):
     dic = {}
     for i, num in enumerate(A):
@@ -5,8 +7,22 @@ def two_sum(A, target):
         if(n not in dic):
             dic[num] = i
         else:
-            return [dic[n],i]
+            return [A[dic[n]],A[i]]
+
+def two_sum_2(arr, target):
+    A.sort()
+    l, r = 0, len(A) - 1
+    while(l < r):
+        sum = A[l] + A[r]
+        if(sum == target):
+            return [A[l],A[r]]
+        elif (sum < target):
+            l += 1
+        else:
+            r -= 1
+    return False
 
 A = [4,6,1,9,0,5,3]
 target = 8
 print(two_sum(A, target))
+print(two_sum_2(A, target))
