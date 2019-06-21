e484ea5b935ac57a9dc85a44962e19933bcedf23
Charles <charlespatel07@gmail.com>
2019-06-21 16:01:03 -0700

    path sum 2

--- trees/path_sum.py
+++ trees/path_sum.py
@@ -25,16 +25,49 @@ def hasPathSum(root, sum):
             path.append((node.right, curr_sum - node.right.val))
     return False
 
+
+# Given a binary tree and a sum, find all root-to-leaf paths where
+# each path's sum equals the given sum.
+
+
+def pathSum(root, sum):
+    paths = []
+    cur_path = []
+
+    def allPath(root, sum):
+        if root is None:
+            return False
+
+        cur_path.append(root.val)
+        sum -= root.val
+        if(root.left == None and root.right == None and sum == 0):
+            paths.append(list(cur_path))
+        else:
+            allPath(root.left, sum)
+            allPath(root.right, sum)
+        cur_path.pop()
+
+    allPath(root, sum)
+    return paths
+
+
 if(__name__ == "__main__"):
-    sum = 9
-    root = Node(1)
-    root.left = Node(2)
-    root.right = Node(3)
-    root.left.left = Node(4)
-    root.right.left = Node(5)
-    root.right.right = Node(7)
-    root.left.left.left = Node(10)
-    root.left.left.right = Node(11)
-    root.right.right.left = Node(8)
+    sum = 22
+    root = Node(5)
+
+    root.left = Node(4)
+    root.right = Node(8)
+
+    root.left.left = Node(11)
+
+    root.right.left = Node(13)
+    root.right.right = Node(4)
+
+    root.left.left.left = Node(7)
+    root.left.left.right = Node(2)
+
+    root.right.right.left = Node(5)
+    root.right.right.right = Node(1)
 
     print(hasPathSum(root, sum))
+    print(pathSum(root, sum))
