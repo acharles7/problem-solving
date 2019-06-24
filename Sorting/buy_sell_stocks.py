f64a6a40e0b3ede10a53853082dcb9d5cac7c270
Charles <charlespatel07@gmail.com>
2019-06-23 19:26:16 -0700

    buy sell stock

--- Sorting/buy_sell_stocks.py
+++ Sorting/buy_sell_stocks.py
@@ -1,14 +1,20 @@
+# If you were only permitted to complete at most one transaction
+# (i.e., buy one and sell one share of the stock),
+# Design an algorithm to find the maximum profit.
+
+# Note that you cannot sell a stock before you buy one.
+
 import sys
-def maxProfit(A):
+def maxProfit(stocks):
     min_stock = sys.maxsize
     profit = 0
 
-    for stock in A:
+    for stock in stocks:
         if(stock < min_stock):
             min_stock = stock
         elif(stock - min_stock > profit):
             profit = stock - min_stock
     return profit
-            
-A = [7,1,5,3,6,4]
-print(maxProfit(A))
+
+stocks = [7,1,5,3,6,4]
+print(maxProfit(stocks))
