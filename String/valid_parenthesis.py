04ec326a57794d8c05028128b979cd0fd6809625
Charles <charlespatel07@gmail.com>
2019-06-27 12:50:34 -0700

    valid parenthesis

---
+++ String/valid_parenthesis.py
@@ -0,0 +1,18 @@
+def isValid(s):
+    stack = []
+    braces = {'{':'}','[':']','(':')'}
+
+    for i in s:
+        if i in braces:
+            stack.append(i)
+        else:
+            if stack:
+                top = stack.pop()
+            else:
+                top = '#'
+            if braces[top] != i:
+                return False
+    return not stack
+
+s = "{[]}"
+print(isValid(s))
