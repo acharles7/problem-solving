14637986f1c46a74e7c5575fa29027bd4ad5c50c
Charles <charlespatel07@gmail.com>
2019-07-08 12:41:36 -0700

    phone number

--- String/combinations_phone_number.py
+++ String/combinations_phone_number.py
@@ -1,6 +1,5 @@
 #Letter Combinations of a Phone Number
 
-
 def letterCombinations(digits: str):
     nums = [[2, 'abc'],[3, 'def'],[4, 'ghi'],[5, 'jkl'], [6, 'mno'], [7, 'pqrs'], [8, 'tuv'], [9, 'wxyz']]
     out = []
