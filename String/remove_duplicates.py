544d1baba1d00158ecd162e6f744025437bfe215
Charles <charlespatel07@gmail.com>
2019-07-08 12:40:56 -0700

    remove duplicates

---
+++ String/remove_duplicates.py
@@ -0,0 +1,12 @@
+def removeDuplicates(s):
+    output = []
+
+    for char in s:
+        if len(output) > 0 and char == output[-1]:
+            output.pop()
+        else:
+            output.append(char)
+    return ''.join(output)
+
+s = "dabbadca"
+print(removeDuplicates(s))
