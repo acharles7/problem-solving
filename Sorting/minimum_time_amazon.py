b726f7a40944fc679f6aa9f99c1642f019c71f84
Charles <charlespatel07@gmail.com>
2019-07-21 13:28:26 -0700

    minimum time

--- Sorting/minimum_time_amazon.py
+++ Sorting/minimum_time_amazon.py
@@ -1,19 +1,19 @@
-def minimumTime(files):
+# Algoritm to find minimum possible time to merge N subfiles into a single file
 
+def minimumTime(files):
     files.sort()
 
     time = files[0]
     output = []
-
     for i in range(1, len(files)):
         time += files[i]
         output.append(time)
-    print(output)
     return sum(output)
 
+# Optimal
 def minimumTime2(files):
     files.sort()
-    print(files)
+
     output = []
     l, r = 0, len(files) - 1
     while(l < r):
@@ -21,8 +21,8 @@ def minimumTime2(files):
         output.append(time)
         l += 1
         r -= 1
-    print(output)
     return sum(output)
-files = [20, 4, 8, 2]
-print(minimumTime2(files))
-# print(minimumTime(files))
+files = [1, 2, 5, 10, 35, 89]
+
+print("Optimal:", minimumTime2(files))
+print("Less Optimal:", minimumTime(files))
