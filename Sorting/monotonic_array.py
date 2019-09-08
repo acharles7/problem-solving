3fae173767a7af3874ab531f930975f2b16d513f
Charles <charlespatel07@gmail.com>
2019-09-07 22:59:12 -0700

    monotonic array

---
+++ Sorting/monotonic_array.py
@@ -0,0 +1,15 @@
+# An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+def monotonic(A):
+    increasing = True
+    decreasing = True
+
+    for i in range(len(A)-1):
+        if A[i] > A[i+1]:
+            increasing = False
+        if A[i] < A[i+1]:
+            decreasing = False
+    return increasing or decreasing
+
+A = [1,2,2,3]
+print(monotonic(A))
