e0b39a9aa7a842f351b3800ccbc3347b3aeb481e
Charles <charlespatel07@gmail.com>
2019-09-23 14:11:27 -0700

    change 2 sum

--- Amazon Interview/two_sum.py
+++ Amazon Interview/two_sum.py
@@ -1,18 +1,18 @@
 
 def twoSum(nums, target):
-    answer, nums = set(), set(nums)
+    answer, complement = set(), set()
     for num in nums:
         comp = target - num
-        if comp in nums:
+        if comp in complement:
             if num > comp:
                 res = (num, comp)
             else:
                 res = (comp, num)
             if res not in answer:
                 answer.add(res)
+        complement.add(num)
     return (answer, len(answer))
 
-
-nums = [1, 1, 2, 2]
-target = 3
+nums = [0,4,1,6,2,9,3,5,1,7]
+target = 6
 print(twoSum(nums, target))
